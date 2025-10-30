7829676df1d39b9b9119883ab651467b2d53e07f
skansher <117953654+skansher@users.noreply.github.com>
2025-10-30 09:26:30 -0400

    Connected news connections

--- src/Components/News.jsx
+++ src/Components/News.jsx
@@ -1,15 +1,35 @@
-
+import React, { useState, useEffect } from 'react';
+import newsData from '../data/news.json';
 import Header from "./Header.jsx";
 import Card from "../Components/NewsCard.jsx";
 
 const News = () => {
+    const [article, setArticles] = useState([]);
+
+    useEffect(() => {
+    const newsArray = Object.entries(newsData).map(([key, value]) => ({
+      newsKey: key,
+      ...value[0],
+    }));
+
+    setArticles(newsArray);
+      }, []);
     return (
-        <div className="Home">
+    <div>
       <Header />
-      <header className="App-header">
-        News
-      </header>
-      <Card />
+      <div className="page-container">
+        <h1>Recent Cyber Attacks</h1>
+
+        <div className="news-container">
+          {article.length > 0 ? (
+            article.map((art, index) => (
+              <Card key={index} news={art} index={index} />
+            ))
+          ) : (
+            <p>No articles found</p>
+          )}
+        </div>
+      </div>
     </div>
     );
 }
--- src/Components/NewsCard.jsx
+++ src/Components/NewsCard.jsx
@@ -1,13 +1,28 @@
 
+import React, { useState } from "react";
 import "../CSS/newscard.css";
 
-const NewsCard = () => {
+const NewsCard = ({news, index}) => {
+    // Determine color class based on index
+    const colorClass = `color-${index % 3}`;
+
     return (
-        <div className="card">
-            <div className="title1"> Article Name </div>
-                <div className="profile-details"> News name (channel, forum, etc.) </div>
-                <div className="profile-details"> Date </div>
-                <div className="profile-details"> Summary </div>
+        <div className={`news-card ${colorClass}`}>
+           {/* Title */}
+           <h2 className="card-title">{news["Article Name"] || 'N/A'}</h2>
+
+                <div className="news-details">
+                    <label>{news["News Channel"] || 'N/A'}</label>
+                </div>
+                <div className="news-details">
+                    <label>{news["Published Date"] || 'N/A'}</label>
+                </div>
+                <div className="news-details">
+                    <label>{news["Summary"] || 'N/A'}</label>
+                </div>
+                <div className="news-details">
+                    <label>{news["URL"] || 'N/A'}</label>
+                </div>
         </div>
     );
 }
